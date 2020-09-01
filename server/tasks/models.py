91af1ad4685b37dccadefaa34fce5d2f9e3d0860
Luis Arce <luis.arce22@gmail.com>
2020-09-01 09:55:14 -0400

    register task model

--- server/tasks/models.py
+++ server/tasks/models.py
@@ -4,10 +4,9 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 
-
 class Task(models.Model):
-    create_by = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
-    assigned_to = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    # create_by = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    # assigned_to = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
     departament = models.IntegerField(default=0)
     title = models.CharField(max_length=250, default='')
     description = models.CharField(max_length=250, default='')
