6530c799fe929d836c991afe27a173ce2e680fc3
Luis Arce <luis.arce22@gmail.com>
2020-09-01 14:58:29 -0400

    save create by and assigned to

--- server/tasks/models.py
+++ server/tasks/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Task(models.Model):
-    # create_by = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
-    # assigned_to = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='created_by')
+    assigned_to = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='assigned_to')
     departament = models.IntegerField(default=0)
     title = models.CharField(max_length=250, default='')
     description = models.CharField(max_length=250, default='')
