49c22412179cdfc2b83ad8b85fa5c897dcae01e5
Luis Arce <luis.arce22@gmail.com>
2020-09-01 20:49:42 -0400

    components to manage tasks

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './comp/tasks/task-list/task-list.component';
+import { TaskCreateComponent } from './comp/tasks/task-create/task-create.component';
+import { TaskViewComponent } from './comp/tasks/task-view/task-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskCreateComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule,
