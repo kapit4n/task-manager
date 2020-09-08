9fe05d9ace8346873509f2db67b9ca36f96b8fb8
Luis Arce <luis.arce22@gmail.com>
2020-09-07 21:45:52 -0400

    ui: login page form

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TaskCreateComponent } from './comp/tasks/task-create/task-create.component';
 import { TaskListComponent } from './comp/tasks/task-list/task-list.component';
+import { LoginComponent } from './comp/auth/login/login.component';
 
 
 const routes: Routes = [
   {
     path: '', component: TaskListComponent
+  },
+  {
+    path: 'login', component: LoginComponent
   },
   {
     path: 'tasks/create', component: TaskCreateComponent
--- ui/src/app/comp/auth/login/login.component.ts
+++ ui/src/app/comp/auth/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+  }
+
+  onCancel() {
   }
 
 }
