466399b21ac85b3dcba4d0bd9242ec76b9871e2f
Juan Flores Vargas <juan.flores.vargas@gmail.com>
2020-09-10 20:41:25 -0400

    update task create

--- ui/src/app/shared/_services/departments.service.ts
+++ ui/src/app/shared/_services/departments.service.ts
@@ -1,20 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Department} from 'src/app/shared/_models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentsService {
-
-  baseUrl = "http://localhost:8000/api";
-
   constructor(private http: HttpClient) {
 
   }
 
   public list() {
-    return this.http.get<Department[]>(`${this.baseUrl}/departments`);
+    return this.http.get<Department[]>(`/departments`);
   }
 }
